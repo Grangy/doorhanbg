9ac91685ccc4805356cd76400d11dd45c246c193
Bogdan <zarechnyy00@internet.ru>
2025-10-12 16:05:05 +0300

    second commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,10 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
-
-  // Получаем поддомен из заголовка host (более надежно)
   const host = request.headers.get('host') || '';
 
   const subdomain = host.split('.')[0];
 
-  // Определяем регион по поддомену
   let region = 'default';
 
   if (subdomain === 'simferopol') region = 'simferopol';
@@ -17,15 +14,9 @@ export function middleware(request: NextRequest) {
   else if (subdomain === 'alushta') region = 'alushta';
   else if (subdomain === 'sevastopol') region = 'sevastopol';
 
-  // Передаем регион в заголовках
   response.headers.set('x-region', region);
   response.headers.set('x-pathname', request.nextUrl.pathname);
-
-  console.log('📤 Headers set:', {
-    'x-region': region,
-    'x-pathname': request.nextUrl.pathname,
-  });
-
+  console.log(request.nextUrl.pathname);
   return response;
 }
 
