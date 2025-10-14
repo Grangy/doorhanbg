85d905d7975b017db8b6a29cb04b555839801bae
Bogdan <zarechnyy00@internet.ru>
2025-10-14 22:55:40 +0300

    deploy gh pages 2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,39 +1,27 @@
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';
+
+// Удалите эту строку:
+// export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://crimea-doorhan.ru';
-
   return [
     {
-      url: baseUrl,
+      url: 'https://crimea-doorhan.ru',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/#products`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#about`,
+      url: 'https://crimea-doorhan.ru/categories',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/#contacts`,
+      url: 'https://crimea-doorhan.ru/products',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#services`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
     },
   ];
 }
-
